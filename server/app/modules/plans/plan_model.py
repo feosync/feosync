81139f6421e662132d42346ad54d2fadea7ff363
THE-AUDIO <nandraina.dev22@gmail.com>
2026-03-13 09:02:21 +0300

    features/add_plan

--- server/app/modules/plans/plan_model.py
+++ server/app/modules/plans/plan_model.py
@@ -8,8 +8,9 @@ class Plan(Base):
     __tablename__ = "plans"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=False)
     price: Mapped[float] = mapped_column(Float, nullable=False)
     features: Mapped[List[str]] = mapped_column(ARRAY(String), nullable=False)
-
-
+    max_page: Mapped[int] = mapped_column(Integer, nullable=False)
+    max_post_month:Mapped[int] = mapped_column(Integer, nullable=False)  
+    max_ai_gen: Mapped[int] = mapped_column(Integer, nullable=False)
+    user = relationship("User", back_populates="plan")
--- server/app/modules/plans/router.py
+++ server/app/modules/plans/router.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from app.core.database import get_db
+from sqlalchemy.orm import Session
+from .schema import PlanInputOutput
+from .plan_repository import PlanRepository as plan_repository
+from app.modules.auth.dependencies import get_active_user
+from app.modules.user.user_model import User
 
 plans_router = APIRouter()
 @plans_router.get("/plans")
 async def get_plans():
     return {"message": "List of plans"} 
+
+@plans_router.post("/add")
+def add_plan(
+    plan: PlanInputOutput,
+    db: Session = Depends(get_db),
+    user: User =Depends(get_active_user)):
+    return  plan_repository.add_plan(plan, db)
