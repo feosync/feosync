86de62d8521ef0666793044a932b0e53c79e20fb
THE-AUDIO <nandraina.dev22@gmail.com>
2026-03-17 20:09:44 +0300

    remove schedule

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -32,7 +32,6 @@ from app.modules.scheduled_post.models.scheduled_post_model import ScheduledPost
 from app.modules.published_post.model import PublishedPost
 from app.modules.post_analytics.model import PostAnalytics
 from app.modules.refresh_token.refresh_token_model import FreshToken
-from app.modules.schedule.model import Schedule
 from app.modules.scheduled_post.models.scheduled_post_ai_image import ScheduledPostAiImage
 
 target_metadata =  Base.metadata
