3882dea9c327d0da90e33998de3c3edaaf84ff3b
THE-AUDIO <nandraina.dev22@gmail.com>
2026-03-17 16:01:51 +0300

    add scheduled_post at schedule

--- server/app/modules/scheduled_post/schemas/scheduled_post_schema.py
+++ server/app/modules/scheduled_post/schemas/scheduled_post_schema.py
@@ -8,30 +8,10 @@ from app.modules.scheduled_post.models.scheduled_post_model import PostStatus
 from typing import Optional
 
 
-
 class ScheduledPostResponse(BaseModel):
     id: UUID
-    caption: str
-    content: dict
-    image_url: str | None
-    publish_at: datetime
-    status: PostStatus
-    post_template_id: UUID | None
-    schedule_id: UUID
-    model_config = {"from_attributes": True}
-    
-
-class ScheduledPostUpdate(BaseModel):
-    caption: Optional[str] | None
-    content: Optional[dict] | None
-    image_url: Optional[str] | None
-    status: PostStatus = PostStatus.SCHEDULED
-    post_template_id: Optional[UUID] | None# app/modules/scheduled_post/schemas.py
-
-class ScheduledPostResponse(BaseModel):
-    id: UUID
-    caption: str
-    content: dict
+    caption: str | None
+    content: dict | None
     image_url: str | None
     publish_at: datetime
     status: PostStatus
@@ -52,24 +32,12 @@ class ScheduledPostUpdate(BaseModel):
 
 class ScheduledPostCreate(BaseModel):
     schedule_id:UUID
-    caption:str 
-    content: dict
-    image_url: Optional[str] | None
+    caption:Optional[str] | None = None
+    content: Optional[dict] | None = None
+    image_url: Optional[str] | None = None
     status: PostStatus = PostStatus.SCHEDULED
-    post_template_id: Optional[UUID] | None
-    publish_at: datetime
-    publish_at: Optional[datetime] | None
-
-
-
-class ScheduledPostCreate(BaseModel):
-    schedule_id:UUID
-    caption:str 
-    content: dict
-    image_url: Optional[str] | None
-    status: PostStatus = PostStatus.SCHEDULED
-    post_template_id: Optional[UUID] | None
-    publish_at: datetime
+    post_template_id: Optional[UUID] | None = None
+    publish_at: datetime = datetime.now(UTC)
     
 class generateImageCreate(BaseModel):
     id:int
