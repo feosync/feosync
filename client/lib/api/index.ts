93aebdeea6c07c554051501d7a1051abb091d69c
AnicetJonhia <anicet22.aps2a@gmail.com>
2026-03-21 13:24:25 +0300

    dev/client deleting non used files

--- client/lib/api/index.ts
+++ client/lib/api/index.ts
@@ -2,6 +2,3 @@
 export * from './types';
 export * from './schemas';
 export { apiClient } from './client';
-export * from './hooks';
-export { API_CONFIG, isUsingMockApi } from './config';
-export * from './mock';
