45fba102daf60c52f59bb34017c6360d94996132
THE-AUDIO <nandraina.dev22@gmail.com>
2026-03-30 13:03:42 +0300

    update a generate comment at comment service

--- server/app/modules/ai_generation/comment_service.py
+++ server/app/modules/ai_generation/comment_service.py
@@ -1,11 +1,24 @@
-from .llm_service import GeminiProvider
+from .llm_service import GeminiProvider as gemini_provider
+from app.modules.published_post.service import PublishedPostService
 from app.modules.published_post.model import PublishedPost
-class CommentService:
-    def __init__(self):
-        self.llm = GeminiProvider()
-        self.published_post:PublishedPost
+from app.modules.scheduled_post.service import ScheduledPostService as scheduled_post_service
+from app.modules.scheduled_post.models.scheduled_post_model import ScheduledPost
+from sqlalchemy.orm import Session
 
-    async def generate_comment(self, comments: str) -> str:
-        question = f"Génère un commentaire pertinent et engageant pour ce post en ce basant sur l'instruction suivante. \n commentaire : {comments}  , instruction: {self.published_post.instructions}"
-        comment, _ = await self.llm.generate_caption(question)
-        return comment
+class CommentService:
+   def __init__(self):
+       self.llm = gemini_provider
+       self.publised_post: PublishedPost 
+  
+
+   async def comment_classification(self, comment:str)-> str:
+       prompt = f"classifier ce commentaire parmis le mot clés suivants: {self.publised_post.keywords} et retourne le mot clé correspondant. Commentaire: {comment}"
+       classification = await self.llm.generate_response(prompt)
+       print(f"classification: {classification}")
+       return classification.strip()
+  
+   async def generate_reply(self, comment:str, db:Session)-> str:
+       scheduled_post = scheduled_post_service.get_by_id(db=db, post_id=self.publised_post.id)
+       prompt = f"Génère une réponse pertinente et engageante pour un commentaire d'une publication à base du contexte suivant: {self.publised_post.instructions} et la légende: {scheduled_post.caption} . Commentaire: {comment}"
+       reply:str = await self.llm.generate_response(prompt)
+       return reply.strip()
