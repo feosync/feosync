e55cf2f505f4d010530ea962dd9e1e3b93234acd
THE-AUDIO <nandraina.dev22@gmail.com>
2026-04-03 17:19:21 +0300

    update hover button for dark mode

--- client/components/layout/Navbar.tsx
+++ client/components/layout/Navbar.tsx
@@ -59,7 +59,7 @@ export const Navbar = () => {
             variant="ghost"
             size="icon"
             onClick={toggle}
-            className="text-slate-600 dark:text-slate-400 hover:text-slate-900 dark:hover:text-white"
+            className="text-slate-600 dark:text-slate-400  hover:bg-slate-100 dark:hover:bg-slate-800"
             aria-label="Basculer le thème"
           >
             {dark
