a926fe2e110ce96b79f7b9a82213e1dd834d7e4a
AnicetJonhia <anicet22.aps2a@gmail.com>
2026-05-17 12:04:44 +0300

    chore(auth):   update cookiies config

--- client/lib/config.ts
+++ client/lib/config.ts
@@ -3,12 +3,12 @@ export const config = {
   appUrl: process.env.NEXT_PUBLIC_APP_URL!,
   googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
   stripePublishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
-  apiUrl: "",    
+  
   stripeSecretKey: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!,
   appEnv: process.env.NEXT_PUBLIC_APP_ENV!,
-  ...(process.env.NEXT_PUBLIC_APP_ENV === "production" && {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL!,
-  }),
+  
+  apiUrl: process.env.NEXT_PUBLIC_API_URL!
+
 } as const;
 
 
